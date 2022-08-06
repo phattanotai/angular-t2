b51afd34844b26719cf5b65468ea4d743aac6fd9
phattanothai <zeref38@gmail,com>
2022-08-07 05:45:21 +0700

    update sonarqube

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,14 +3,11 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit, OnDestroy {
   constructor() {}
 
-  ngOnInit() {
-  }
-  ngOnDestroy() {
-  }
-
+  ngOnInit() {}
+  ngOnDestroy() {}
 }
